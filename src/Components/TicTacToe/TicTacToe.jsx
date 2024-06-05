2addce390db262dcd73c45c78a6d56c892b4ed08
Victor Onyekwere <cydollarsign@gmail.com>
2024-06-05 23:23:45 +0100

    commit

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TicTacToe.css'
+import circle from '../Assets/O-icon.png'
+import cross from'../Assets/X-icon.png'
+
+const TicTacToe = () => {
+  return (
+    <div>TicTacToe</div>
+  )
+}
+
+export default TicTacToe
