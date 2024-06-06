6dfef2d72f93e918e251919199d0bdc4f746ab9b
Victor Onyekwere <cydollarsign@gmail.com>
2024-06-06 15:58:35 +0100

    Createted TicTacToe Boxes

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -5,7 +5,28 @@ import cross from'../Assets/X-icon.png'
 
 const TicTacToe = () => {
   return (
-    <div>TicTacToe</div>
+    <div className='container'>
+      <h1 className="title">Tic Tac <span> Toe</span></h1>
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+
+      </div>
+      <button className="reset">Reset</button>
+    </div>
   )
 }
 
