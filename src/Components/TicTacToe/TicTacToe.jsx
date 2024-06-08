85f370a065633c4c1ba2b4138c38843818acd010
Victor Onyekwere <cydollarsign@gmail.com>
2024-06-08 21:58:37 +0100

    Click Buttons

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,27 +1,38 @@
 import React, { useState } from 'react'
 import './TicTacToe.css'
 import circle from '../Assets/O-icon.png'
-import cross from'../Assets/X-icon.png'
+import cross from '../Assets/X-icon.png'
 
 
 const TicTacToe = () => {
 
   let [count, setCount] = useState(0);
   let [lock, setLock] = useState(false);
-  let [data, setData] = useState(["","","","","","","","",""]);
+  let [data, setData] = useState(["", "", "", "", "", "", "", "", ""]);
 
-  function tojjle (e,num) {
+  let createBoxes = () => {
+    for (let i = 0; i < 3; i++) {
+      let box = document.createElement("div");
+      console.log(box)
+      alert(box)
+    }
+  }
+
+  createBoxes()
+  // console.log(box)
+
+  function tojjle(e, num) {
     if (lock) {
       return 0
     }
-    if (count%2===0) {
+    if (count % 2 === 0) {
       e.target.innerHTML = `<img src='${cross}'>`
       data[num] = 'x';
-    } else { 
+    } else {
       e.target.innerHTML = `<img src='${circle}'>`
       data[num] = 'o';
     }
-    setCount(count+1);
+    setCount(count + 1);
     console.log(e.target, data, num)
 
   }
@@ -30,19 +41,19 @@ const TicTacToe = () => {
       <h1 className="title">Tic Tac <span> Toe</span></h1>
       <div className="board">
         <div className="row1">
-          <div onClick={(e) => {tojjle(e,0)}} className="boxes"></div>
-          <div onClick={(e) => {tojjle(e,1)}} className="boxes"></div>
-          <div onClick={(e) => {tojjle(e,2)}} className="boxes"></div>
+          <div onClick={(e) => { tojjle(e, 0) }} className="boxes"></div>
+          <div onClick={(e) => { tojjle(e, 1) }} className="boxes"></div>
+          <div onClick={(e) => { tojjle(e, 2) }} className="boxes"></div>
         </div>
         <div className="row2">
-          <div onClick={(e) => {tojjle(e,3)}} className="boxes"></div>
-          <div onClick={(e) => {tojjle(e,4)}} className="boxes"></div>
-          <div onClick={(e) => {tojjle(e,5)}} className="boxes"></div>
+          <div onClick={(e) => { tojjle(e, 3) }} className="boxes"></div>
+          <div onClick={(e) => { tojjle(e, 4) }} className="boxes"></div>
+          <div onClick={(e) => { tojjle(e, 5) }} className="boxes"></div>
         </div>
         <div className="row3">
-          <div onClick={(e) => {tojjle(e,6)}} className="boxes"></div>
-          <div onClick={(e) => {tojjle(e,7)}} className="boxes"></div>
-          <div onClick={(e) => {tojjle(e,8)}} className="boxes"></div>
+          <div onClick={(e) => { tojjle(e, 6) }} className="boxes"></div>
+          <div onClick={(e) => { tojjle(e, 7) }} className="boxes"></div>
+          <div onClick={(e) => { tojjle(e, 8) }} className="boxes"></div>
         </div>
 
       </div>
